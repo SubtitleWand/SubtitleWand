653e7ce3f48f69cd44eee79df70707fbd042d5cd
Dowen <tokenyete@gmail.com>
2020-03-23 23:53:49 +0800

    ci

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -13,8 +13,8 @@ if grep -q 'sdk: flutter' "./pubspec.yaml"; then
     flutter analyze --no-current-package lib
 
     if [[ -d "test" ]]; then
-        flutter format --set-exit-if-changed test
-        #flutter analyze --no-current-package test #
+        #flutter format --set-exit-if-changed test
+        flutter analyze --no-current-package test #
         flutter test --no-pub --coverage
     fi
 fi
